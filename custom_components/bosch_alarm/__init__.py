5d1ece480cd632098d29aa64a2d15da13488cb07
Sanjay Govind <sanjay.govind9@gmail.com>
2023-05-30 21:49:43 +1200

    migrate history to its own sensor

--- custom_components/bosch_alarm/__init__.py
+++ custom_components/bosch_alarm/__init__.py
@@ -20,7 +20,7 @@ from .const import (
     DOMAIN
 )
 
-PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.ALARM_CONTROL_PANEL]
+PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.ALARM_CONTROL_PANEL, Platform.SENSOR]
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
