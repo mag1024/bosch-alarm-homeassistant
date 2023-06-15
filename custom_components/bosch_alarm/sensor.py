e97d16bf301efa7d8b2454f3d472b2e9c1338352
Sanjay Govind <sanjay.govind9@gmail.com>
2023-06-15 23:32:01 +1200

    use sync version of scheduled_update_ha_state

--- custom_components/bosch_alarm/sensor.py
+++ custom_components/bosch_alarm/sensor.py
@@ -49,10 +49,10 @@ class PanelHistorySensor(SensorEntity):
         return { HISTORY_ATTR: "\n".join(x[1] for x in history)}
     
     async def async_added_to_hass(self):
-        self._observer.attach(self.async_schedule_update_ha_state)
+        self._observer.attach(self.schedule_update_ha_state)
 
     async def async_will_remove_from_hass(self):
-        self._observer.detach(self.async_schedule_update_ha_state)
+        self._observer.detach(self.schedule_update_ha_state)
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up a sensor for tracking panel history."""
