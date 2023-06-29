aa92cb78e781e5620a5f3199c47bc8ed0da38fdb
Sanjay Govind <sanjay.govind9@gmail.com>
2023-06-29 17:38:34 +1200

    remove previous history

--- custom_components/bosch_alarm/__init__.py
+++ custom_components/bosch_alarm/__init__.py
@@ -26,7 +26,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Bosch Alarm from a config entry."""
     panel = bosch_alarm_mode2.Panel(
             host=entry.data[CONF_HOST], port=entry.data[CONF_PORT],
-            passcode=entry.data[CONF_PASSWORD])   
+            passcode=entry.data[CONF_PASSWORD])
     try:
         await panel.connect()
     except asyncio.exceptions.TimeoutError:
