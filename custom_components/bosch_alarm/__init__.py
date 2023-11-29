135465bf0c08c77ac4fef1859fb5f4583ecb506c
Sanjay Govind <sanjay.govind9@gmail.com>
2023-11-29 20:07:59 +1300

    Add a service call to set the panels date and time

--- custom_components/bosch_alarm/__init__.py
+++ custom_components/bosch_alarm/__init__.py
@@ -8,21 +8,30 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry
+from homeassistant.helpers import device_registry, config_validation
 from homeassistant.util import dt
+from homeassistant.helpers.service import async_extract_config_entry_ids
+import voluptuous as vol
+import datetime
 
 from homeassistant.const import (
     CONF_HOST,
     CONF_PORT,
-    CONF_PASSWORD
+    CONF_PASSWORD,
+    ATTR_ENTITY_ID
 )
 
 import bosch_alarm_mode2
 
-from .const import DOMAIN, CONF_INSTALLER_CODE, CONF_USER_CODE
+from .const import DOMAIN, CONF_INSTALLER_CODE, CONF_USER_CODE, ATTR_DATETIME
 
 PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.ALARM_CONTROL_PANEL, Platform.SENSOR,
                              Platform.SWITCH]
+SET_DATE_TIME_SERVICE_NAME = "set_date_time"
+SET_DATE_TIME_SCHEMA = vol.Schema({
+    vol.Optional(ATTR_DATETIME): config_validation.datetime,
+    ATTR_ENTITY_ID: config_validation.entity_ids
+})
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -40,8 +49,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = panel
-    def set_panel_date(call):
-        return panel.set_panel_date(dt.utcnow())
+    async def set_panel_date(service_call):
+        value: datetime = service_call.data.get(ATTR_DATETIME, dt.utcnow())
+        for entry_id in await async_extract_config_entry_ids(hass, service_call):
+            entry_panel = hass.data[DOMAIN][entry_id]
+            await entry_panel.set_panel_date(value)
+
 
     def setup():
         # Some panels don't support retrieving a serial number.
@@ -55,8 +68,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         for device_entry in device_registry.async_entries_for_config_entry(dr, entry.entry_id):
             if (DOMAIN, panel.model) in device_entry.identifiers:
                 dr.async_remove_device(device_entry.id)
-
-        hass.services.register(DOMAIN, "set_date_time", set_panel_date)
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setups(entry, PLATFORMS))
     if panel.connection_status():
@@ -66,6 +77,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
                 lambda: panel.connection_status() and setup())
 
     entry.async_on_unload(entry.add_update_listener(options_update_listener))
+
+    hass.services.async_register(DOMAIN, SET_DATE_TIME_SERVICE_NAME, set_panel_date, SET_DATE_TIME_SCHEMA)
     return True
 
 async def options_update_listener(
--- custom_components/bosch_alarm/const.py
+++ custom_components/bosch_alarm/const.py
@@ -4,3 +4,4 @@ DOMAIN = 'bosch_alarm'
 HISTORY_ATTR = 'history'
 CONF_INSTALLER_CODE = 'installer_code'
 CONF_USER_CODE = 'user_code'
+ATTR_DATETIME = "datetime"
