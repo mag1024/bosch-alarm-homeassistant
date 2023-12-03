2b2e57077769d23255c006feb0a83b3d099047cb
Sanjay Govind <sanjay.govind9@gmail.com>
2023-12-03 20:15:27 +1300

    Add service call for setting the panels date/time (#25)
    * Add a service call to set the panels date and time
    
    * Bump version to 0.3.30
    
    * Add a service call to set the panels date and time
    
    * Add a service call to set the panels date and time
    
    * use entity service
    
    * use entity service
    
    * use entity service
    
    * use entity service
    
    * Fix formatting issues
    
    * Fix attr order
    
    * Fix attr order
    
    * move attr to alarm_control_panel
    
    * Add integration stanza to services yaml

--- custom_components/bosch_alarm/alarm_control_panel.py
+++ custom_components/bosch_alarm/alarm_control_panel.py
@@ -11,6 +11,13 @@ from homeassistant.components.alarm_control_panel import (
 import homeassistant.components.alarm_control_panel as alarm
 
 from homeassistant.const import CONF_CODE
+from homeassistant.helpers import config_validation, entity_platform
+from homeassistant.helpers.config_validation import make_entity_service_schema
+from homeassistant.util import dt
+
+import voluptuous as vol
+import datetime
+from typing import Any
 
 from .const import DOMAIN
 from .device import device_info_from_panel
@@ -23,6 +30,12 @@ READY_STATE_HOME = 'home'
 READY_STATE_AWAY = 'away'
 FAULTED_POINTS_ATTR = 'faulted_points'
 ALARMS_ATTR = 'alarms'
+DATETIME_ATTR = 'datetime'
+
+SET_DATE_TIME_SERVICE_NAME = "set_date_time"
+SET_DATE_TIME_SCHEMA = make_entity_service_schema({
+    vol.Optional(DATETIME_ATTR): config_validation.datetime
+})
 
 class AreaAlarmControlPanel(AlarmControlPanelEntity):
     def __init__(self, panel, arming_code, area_id, area, unique_id):
@@ -99,13 +112,19 @@ class AreaAlarmControlPanel(AlarmControlPanelEntity):
         self._area.alarm_observer.detach(self.schedule_update_ha_state)
         self._area.ready_observer.detach(self.schedule_update_ha_state)
 
+    async def set_panel_date(self, **kwargs: Any):
+        value: datetime = kwargs.get(DATETIME_ATTR, dt.now())
+        await self._panel.set_panel_date(value)
+
 async def async_setup_entry(hass, config_entry, async_add_entities):
     """Set up control panels for each area."""
-
     panel = hass.data[DOMAIN][config_entry.entry_id]
 
     arming_code = config_entry.options.get(CONF_CODE, None)
     async_add_entities(
             AreaAlarmControlPanel(panel, arming_code, id, area, f'{panel.serial_number}_area_{id}')
                 for (id, area) in panel.areas.items())
+
+    platform = entity_platform.async_get_current_platform()
+    platform.async_register_entity_service(SET_DATE_TIME_SERVICE_NAME, SET_DATE_TIME_SCHEMA, "set_panel_date")
 
