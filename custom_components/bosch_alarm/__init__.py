de10420fe7063da41cd93abcddf18d4169964617
Sanjay Govind <sanjay.govind9@gmail.com>
2024-09-07 05:08:46 +1200

    Apply changes from home assistant linting configuration (#45)
    * Fix config_flow so it passes home assistant pylint
    
    * Fix __init__ so it passes pylint
    
    * Fix alarm_control_panel so it passes pylint
    
    * Fix remaining python files so they pass linting

--- custom_components/bosch_alarm/__init__.py
+++ custom_components/bosch_alarm/__init__.py
@@ -2,37 +2,44 @@
 
 from __future__ import annotations
 
-import asyncio
 import logging
-
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers import device_registry
-
-from homeassistant.const import (
-    CONF_HOST,
-    CONF_PORT,
-    CONF_PASSWORD,
-    CONF_MODEL
-)
 
 import bosch_alarm_mode2
 
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    CONF_HOST,
+    CONF_MODEL,
+    CONF_PASSWORD,
+    CONF_PORT,
+    Platform,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
+
+from .const import CONF_INSTALLER_CODE, CONF_USER_CODE, DOMAIN
 from .device import PanelConnection
 
-from .const import DOMAIN, CONF_INSTALLER_CODE, CONF_USER_CODE
-
-PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.ALARM_CONTROL_PANEL, Platform.SENSOR,
-                             Platform.SWITCH, Platform.LOCK]
+PLATFORMS: list[Platform] = [
+    Platform.ALARM_CONTROL_PANEL,
+    Platform.BINARY_SENSOR,
+    Platform.LOCK,
+    Platform.SENSOR,
+    Platform.SWITCH,
+]
 _LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Bosch Alarm from a config entry."""
     panel = bosch_alarm_mode2.Panel(
-            host=entry.data[CONF_HOST], port=entry.data[CONF_PORT],
-            automation_code=entry.data.get(CONF_PASSWORD, None),
-            installer_or_user_code=entry.data.get(CONF_INSTALLER_CODE, entry.data.get(CONF_USER_CODE, None)))
+        host=entry.data[CONF_HOST],
+        port=entry.data[CONF_PORT],
+        automation_code=entry.data.get(CONF_PASSWORD, None),
+        installer_or_user_code=entry.data.get(
+            CONF_INSTALLER_CODE, entry.data.get(CONF_USER_CODE, None)
+        ),
+    )
 
     # The config flow sets the entries unique id to the serial number if available
     # If the panel doesn't expose it's serial number, use the entry id as a unique id instead.
@@ -46,17 +53,18 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     entry.async_on_unload(entry.add_update_listener(options_update_listener))
 
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setups(entry, PLATFORMS))
+        hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    )
     entry.async_create_background_task(hass, panel.connect(), "panel_connection")
     return True
 
-async def options_update_listener(
-    hass: HomeAssistant, config_entry: ConfigEntry
-):
+
+async def options_update_listener(hass: HomeAssistant, config_entry: ConfigEntry):
     """Handle options update."""
     await hass.config_entries.async_reload(config_entry.entry_id)
 
-async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry):
+
+async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Migrate old entry."""
     _LOGGER.debug("Migrating from version %s", config_entry.version)
     new = {**config_entry.data}
@@ -69,12 +77,14 @@ async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry):
 
     if config_entry.version < 3:
         model = config_entry.title.replace("Bosch ", "")
-        
+
         # Remove old devices using the panel model as an identifier
-        dr = device_registry.async_get(hass)
-        for device_entry in device_registry.async_entries_for_config_entry(dr, config_entry.entry_id):
+        device_registry = dr.async_get(hass)
+        for device_entry in dr.async_entries_for_config_entry(
+            device_registry, config_entry.entry_id
+        ):
             if (DOMAIN, model) in device_entry.identifiers:
-                dr.async_remove_device(device_entry.id)
+                device_registry.async_remove_device(device_entry.id)
 
         # The config flow sets the entries title to the panel's model
         new[CONF_MODEL] = model
@@ -85,6 +95,7 @@ async def async_migrate_entry(hass: HomeAssistant, config_entry: ConfigEntry):
     _LOGGER.debug("Migration to version %s successful", config_entry.version)
 
     return True
+
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
